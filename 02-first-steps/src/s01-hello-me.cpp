5cbd3d665aef3fb04c873c7b546d96fddcf2c2ff
s24303 <s24303@pjwstk.edu.pl>
2021-10-15 15:05:31 +0200

    fixing errors

---
+++ 02-first-steps/src/s01-hello-me.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+auto main() -> int {
+  std::cout << "Hello, Adrian!\n";
+  return 0;
+}
---
+++ 02-first-steps/src/s01-hello-name.cpp
@@ -0,0 +1,16 @@
+/*
+ * The very first program you should write in any new programming language.
+ */
+
+#include <iostream>
+#include <string>
+
+auto main() -> int {
+  std::cout << "Jak masz na imię?\n";
+
+  auto name = std::string{};
+  std::getline(std::cin, name);
+
+  std::cout << "Hello, " << name << "!\n";
+  return 0;
+}
