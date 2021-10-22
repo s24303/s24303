bae27678a1f42e11109d17fe33e8f81cbf9b9142
s24303 <s24303@pjwstk.edu.pl>
2021-10-22 15:50:59 +0200

    Update s03-echo.cpp

--- 03-loops/s03-echo.cpp
+++ 03-loops/s03-echo.cpp
@@ -1,27 +1,35 @@
+
 #include <iostream>
 #include <string>
 
+
 auto main(int argc, char*argv[]) -> int
 {
-  //if (argc == 0) return 1;  
-  int x = 1;
-  while (x <= argc) //(int x = 1; x <= argc; x++)
+  
+  for (int i = 1; i < argc; i++)
   { 
     auto var1 = std::string{argv[1]};
     auto var2 = std::string{argv[2]};
-    if (var1 == "-n") 
+    if (var1 == "-n") std::cout << argv[i+1] << " ";
+    else if (var1 == "-l") std::cout << argv[i+1] << std::endl;
+    else if (var1 == "-r" && var2 != "-l" && var2 !="-n") 
     {
-      std::cout << argv[x+1] << " ";
-    }
-    else if (var1 == "-l") std::cout << argv[x+1] << "\n";
-    else if (var1 == "-r") 
-    {
-      for (int x=(argc-1); x>=2; x--) std::cout << argv[x] << " ";
+      for (int i=(argc-1); i>=2; i--) std::cout << argv[i] << " ";
+      std::cout << "\n";
       return 0;
     }
-    else std::cout << argv[x] << " ";
-    x++;
-  };
-  std::cout << "test";
+    else if (var1 == "-r" && var2 == "-l")
+    {
+      for (int i=(argc-1); i>=3; i--) std::cout << argv[i] << "\n";
+      return 0;
+    }
+    else if (var1 == "-r" && var2 == "-n") 
+    {
+      for (int i=(argc-1); i>=3; i--) std::cout << argv[i] << " ";
+      return 0;
+    }
+    else std::cout << argv[i] << " ";
+  }
+ // std::cout << "\n";
   return 0;
 }
