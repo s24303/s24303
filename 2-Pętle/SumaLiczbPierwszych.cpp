e35cd2c95b328c244e8416b7476efbc2fc8f045a
s24303 <s24303@pjwstk.edu.pl>
2021-11-05 15:00:11 +0100

    Update SumaLiczbPierwszych.cpp
    now it's working!

--- 2-Pętle/SumaLiczbPierwszych.cpp
+++ 2-Pętle/SumaLiczbPierwszych.cpp
@@ -5,23 +5,35 @@ int main()
 	int a;
 	std::cout << "Podaj liczbę do sprawdzenia: ";
 	std::cin >> a;
-	for (int i =2; i<a; i++)
+	
+	if (a%2 != 0 && a%3 != 0 && a%5 != 0 && a%7 != 0 && a%11 != 0)
 	{
-		if (a%i==0) 
-		{
-			return 0;
-		}
+		std::cout << "Liczba " << a << " jest pierwsza \n";
 	}
-	for (a; a>0; a--)
+	else if (a == 2 || a == 3 || a == 5 || a == 7 || a == 11)
 	{
-		for (int i =2; i<a; i++)
-		{
-			if (a%i==1)
-			{
-				int suma = suma + a;
-			}
-		}
+		std::cout << "Liczba " << a << " jest pierwsza \n";
 	}
+
+	else 
+	{
+		std::cout << "Liczba " << a << " nie jest pierwsza \n";
+		return 0;
+	}
+
+	int suma = 0;
+	for (int i=a; i>0; i--)
+	{
+		if (i%2 != 0 && i%3 != 0 && i%5 != 0 && i%7 != 0 && i%11)
+		{
+			suma = suma + i;
+		}
+		if (i == 2 || i == 3 || i == 5 || i == 7 || i == 11)
+       		{
+                	suma = suma + i;
+        	} 
+	}
+	std::cout << "Suma liczb pierwszych mniejszych lub równych " << a << " wynosi: " << suma << "\n";
 	return 0;
 }
 
