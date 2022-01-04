bbe802b564dacdb1435e14cc2b2415f80295cc78
s24303 <s24303@pjwstk.edu.pl>
2022-01-04 19:20:20 +0100

    First version of users.cpp

---
+++ 4.1.X/users.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+enum PERMISSION 
+{
+	NO_PERMS,
+	PERM_READ,
+	PERM_WRITE,
+	PERM_READANDWRITE,
+	PERM_EXECUTE,
+	PERM_READANDEXECUTE,
+	PERM_WRITEANDEXECUTE,
+	ALL_PERMS
+};
+
+int main()
+{
+	srand(time(NULL));
+
+	//auto perm = PERM_READ|PERM_WRITE;
+        //auto const can_read = (perm & PERM_READ);
+
+	int users[10]; //tworzenie tablicy
+	for (int i = 0; i < 10; i++)
+	{
+		users[i] = (std::rand()%8); //wypełnienie tablicy losowymi liczbami
+		
+		int perm = users[i];
+		std::cout << "user " << i+1 << ": ";
+		switch (perm)
+		{
+			case NO_PERMS:
+			{
+				std::cout << "have no permissions \n";
+				break;
+        		}
+			case PERM_READ:
+			{
+                                std::cout << "can read \n";
+                                break;
+                        }
+		        case PERM_WRITE:
+			{
+                                std::cout << "can write \n";
+                                break;
+                        }
+		        case PERM_READANDWRITE:
+		        {
+                                std::cout << "can read and write \n";
+                                break;
+                        }
+			case PERM_EXECUTE:
+			{
+                                std::cout << "can execute \n";
+                                break;
+                        }
+		        case PERM_READANDEXECUTE:
+			{
+                                std::cout << "can read and execute \n";
+                                break;
+                        }
+		        case PERM_WRITEANDEXECUTE:
+			{
+                                std::cout << "can write and execute \n";
+                                break;
+                        }
+			case ALL_PERMS:
+			{
+                                std::cout << "have all perms \n";
+                                break;
+                        }
+		}
+	}
+
+	return 0;
+}
