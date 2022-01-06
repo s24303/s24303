c30ca92093057000a5181c45e3e641a0566b55a6
unknown <you@example.com>
2022-01-06 18:08:22 +0100

    student.cpp works with ready database

--- 4.1.X/student.cpp
+++ 4.1.X/student.cpp
@@ -4,80 +4,62 @@
 
 struct student
 {
-	std::string name;
-	std::string surname;
-	std::vector <double> grades;
+        std::string name;
+        std::string surname;
+        std::vector<double> grades;
 };
 
-void students()
+void average_of(std::vector<student> students);
+void get_best_student(std::vector<student> students);
+
+int main()
 {
-	auto stud1 = student {"Adam", "Mickiewicz", {2,3,4,1}};
-	auto stud2 = student {"Jan", "Kochanowski", {4,1,5,3}};
-	auto stud3 = student {"Alex", "Ferguson", {2,4,1,2}};
-	auto students = std::vector<student>();
-       	students.push_back(stud1);
-       	students.push_back(stud2);
-       	students.push_back(stud3);
+        std::vector<student> students;
 
-	for (auto student : students)
-        {
-               	std::cout << student.name << " " << student.surname << " ";
-               	for (auto i = 0; i < (int)student.grades.size(); i++)
-                	std::cout << student.grades[i] << " ";
-		std::cout << "\n";
-        }
+        auto stud1 = student{"Adam", "Mickiewicz", {2, 3, 4, 1}};
+        auto stud2 = student{"Jan", "Kochanowski", {2, 1, 1, 6}};
+        auto stud3 = student{"Alex", "Ferguson", {6, 4, 1, 2}};
 
-}
-
-void average_of()
-{
-	double avg = 1, sum = 0;
-
-	auto stud1 = student {"Adam", "Mickiewicz", {2,3,4,1}};
-        auto stud2 = student {"Jan", "Kochanowski", {4,1,5,3}};
-        auto stud3 = student {"Alex", "Ferguson", {2,4,1,2}};
-        auto students = std::vector<student>();
         students.push_back(stud1);
         students.push_back(stud2);
         students.push_back(stud3);
 
-	for (auto student : students)
-        {
-                for (auto i = 0; i < (int)student.grades.size(); i++)
-                        sum += student.grades[i];
-                avg = sum/(int)student.grades.size();
-		std::cout << "Średnia ucznia " << student.name << " wynosi: " << avg << "\n";
-		sum = 0, avg = 0;
-        }
+        average_of(students);
+        get_best_student(students);
+
+        return 0;
 }
 
-void get_best_student()
+void average_of(std::vector<student> students)
 {
-	double avg = 1, sum = 0;
-
-        auto stud1 = student {"Adam", "Mickiewicz", {2,3,4,1}};
-        auto stud2 = student {"Jan", "Kochanowski", {4,1,5,3}};
-        auto stud3 = student {"Alex", "Ferguson", {2,4,1,2}};
-        auto students = std::vector<student>();
-        students.push_back(stud1);
-        students.push_back(stud2);
-        students.push_back(stud3);
+        double avg = 1, sum = 0;
 
         for (auto student : students)
         {
                 for (auto i = 0; i < (int)student.grades.size(); i++)
                         sum += student.grades[i];
-                avg = sum/(int)student.grades.size();
-                
+                avg = sum / (int)student.grades.size();
+                std::cout << student.name << "'s GPA is " << avg << "\n";
                 sum = 0, avg = 0;
         }
-
 }
 
-int main()
+void get_best_student(std::vector<student> students)
 {
-	students();
-	average_of();
+        double avg = 1, sum = 0, best = 0;
+        std::string best_stud;
 
-	return 0;
+        for (auto student : students)
+        {
+                for (auto i = 0; i < (int)student.grades.size(); i++)
+                        sum += student.grades[i];
+                avg = sum / (int)student.grades.size();
+                if (avg > best)
+                {
+                        best = avg;
+                        best_stud = student.name + " " + student.surname;
+                }
+                sum = 0, avg = 0;
+        }
+        std::cout << best_stud << " is the best student with GPA = " << best << "\n";
 }
