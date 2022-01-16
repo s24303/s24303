d502d4389bed5635aa3363aa80d2525364177865
unknown <you@example.com>
2022-01-16 01:17:40 +0100

    better performance, but looks like shit

--- sudoku/sudoku_working.cpp
+++ sudoku/sudoku_working.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
 
-std::string sudoku[9][9];
+int sudoku[9][9];
 int number, level;
+bool finished = false;
 
 void menu() //menu
 {
@@ -43,87 +44,171 @@ void instruction() // display an instruction
 }
 void easy_lvl() //easy-level sudoku declaration
 {
-    sudoku[0][0] = " ";
-    sudoku[0][1] = " ";
-    sudoku[0][2] = " ";
-    sudoku[0][3] = "6";
-    sudoku[0][4] = " ";
-    sudoku[0][5] = " ";
-    sudoku[0][6] = " ";
-    sudoku[0][7] = "8";
-    sudoku[0][8] = "9";
-    sudoku[1][0] = "6";
-    sudoku[1][1] = " ";
-    sudoku[1][2] = " ";
-    sudoku[1][3] = " ";
-    sudoku[1][4] = " ";
-    sudoku[1][5] = "1";
-    sudoku[1][6] = "4";
-    sudoku[1][7] = " ";
-    sudoku[1][8] = " ";
-    sudoku[2][0] = " ";
-    sudoku[2][1] = " ";
-    sudoku[2][2] = " ";
-    sudoku[2][3] = "3";
-    sudoku[2][4] = "8";
-    sudoku[2][5] = "2";
-    sudoku[2][6] = " ";
-    sudoku[2][7] = "6";
-    sudoku[2][8] = "7";
-    sudoku[3][0] = "3";
-    sudoku[3][1] = "2";
-    sudoku[3][2] = " ";
-    sudoku[3][3] = " ";
-    sudoku[3][4] = " ";
-    sudoku[3][5] = "8";
-    sudoku[3][6] = " ";
-    sudoku[3][7] = " ";
-    sudoku[3][8] = " ";
-    sudoku[4][0] = "7";
-    sudoku[4][1] = " ";
-    sudoku[4][2] = " ";
-    sudoku[4][3] = " ";
-    sudoku[4][4] = " ";
-    sudoku[4][5] = " ";
-    sudoku[4][6] = " ";
-    sudoku[4][7] = " ";
-    sudoku[4][8] = "8";
-    sudoku[5][0] = " ";
-    sudoku[5][1] = " ";
-    sudoku[5][2] = " ";
-    sudoku[5][3] = "7";
-    sudoku[5][4] = " ";
-    sudoku[5][5] = " ";
-    sudoku[5][6] = " ";
-    sudoku[5][7] = "2";
-    sudoku[5][8] = "1";
-    sudoku[6][0] = "8";
-    sudoku[6][1] = "7";
-    sudoku[6][2] = " ";
-    sudoku[6][3] = "5";
-    sudoku[6][4] = "6";
-    sudoku[6][5] = "4";
-    sudoku[6][6] = " ";
-    sudoku[6][7] = " ";
-    sudoku[6][8] = " ";
-    sudoku[7][0] = " ";
-    sudoku[7][1] = " ";
-    sudoku[7][2] = "4";
-    sudoku[7][3] = "1";
-    sudoku[7][4] = " ";
-    sudoku[7][5] = " ";
-    sudoku[7][6] = " ";
-    sudoku[7][7] = " ";
-    sudoku[7][8] = "3";
-    sudoku[8][0] = "2";
-    sudoku[8][1] = "1";
-    sudoku[8][2] = " ";
-    sudoku[8][3] = " ";
-    sudoku[8][4] = " ";
-    sudoku[8][5] = "3";
-    sudoku[8][6] = " ";
-    sudoku[8][7] = " ";
-    sudoku[8][8] = " ";
+    sudoku[0][0];
+    sudoku[0][1];
+    sudoku[0][2];
+    sudoku[0][3] = 6;
+    sudoku[0][4];
+    sudoku[0][5];
+    sudoku[0][6];
+    sudoku[0][7] = 8;
+    sudoku[0][8] = 9;
+    sudoku[1][0] = 6;
+    sudoku[1][1];
+    sudoku[1][2];
+    sudoku[1][3];
+    sudoku[1][4];
+    sudoku[1][5] = 1;
+    sudoku[1][6] = 4;
+    sudoku[1][7];
+    sudoku[1][8];
+    sudoku[2][0];
+    sudoku[2][1];
+    sudoku[2][2];
+    sudoku[2][3] = 3;
+    sudoku[2][4] = 8;
+    sudoku[2][5] = 2;
+    sudoku[2][6];
+    sudoku[2][7] = 6;
+    sudoku[2][8] = 7;
+    sudoku[3][0] = 3;
+    sudoku[3][1] = 2;
+    sudoku[3][2];
+    sudoku[3][3];
+    sudoku[3][4];
+    sudoku[3][5] = 8;
+    sudoku[3][6];
+    sudoku[3][7];
+    sudoku[3][8];
+    sudoku[4][0] = 7;
+    sudoku[4][1];
+    sudoku[4][2];
+    sudoku[4][3];
+    sudoku[4][4];
+    sudoku[4][5];
+    sudoku[4][6];
+    sudoku[4][7];
+    sudoku[4][8] = 8;
+    sudoku[5][0];
+    sudoku[5][1];
+    sudoku[5][2];
+    sudoku[5][3] = 7;
+    sudoku[5][4];
+    sudoku[5][5];
+    sudoku[5][6];
+    sudoku[5][7] = 2;
+    sudoku[5][8];
+    sudoku[6][0] = 8;
+    sudoku[6][1];
+    sudoku[6][2];
+    sudoku[6][3] = 5;
+    sudoku[6][4] = 6;
+    sudoku[6][5] = 4;
+    sudoku[6][6];
+    sudoku[6][7];
+    sudoku[6][8];
+    sudoku[7][0];
+    sudoku[7][1];
+    sudoku[7][2] = 4;
+    sudoku[7][3] = 1;
+    sudoku[7][4];
+    sudoku[7][5];
+    sudoku[7][6];
+    sudoku[7][7];
+    sudoku[7][8] = 3;
+    sudoku[8][0] = 2;
+    sudoku[8][1] = 1;
+    sudoku[8][2];
+    sudoku[8][3];
+    sudoku[8][4];
+    sudoku[8][5] = 3;
+    sudoku[8][6];
+    sudoku[8][7];
+    sudoku[8][8];
+}
+void test_lvl() //test level to check progress
+{
+    sudoku[0][0] = 1;
+    sudoku[0][1] = 3;
+    sudoku[0][2] = 7;
+    sudoku[0][3] = 6;
+    sudoku[0][4] = 4;
+    sudoku[0][5] = 5;
+    sudoku[0][6] = 2;
+    sudoku[0][7] = 8;
+    sudoku[0][8] = 9;
+    sudoku[1][0] = 6;
+    sudoku[1][1] = 8;
+    sudoku[1][2] = 2;
+    sudoku[1][3] = 9;
+    sudoku[1][4] = 7;
+    sudoku[1][5] = 1;
+    sudoku[1][6] = 4;
+    sudoku[1][7] = 3;
+    sudoku[1][8] = 5;
+    sudoku[2][0] = 5;
+    sudoku[2][1] = 4;
+    sudoku[2][2] = 9;
+    sudoku[2][3] = 3;
+    sudoku[2][4] = 8;
+    sudoku[2][5] = 2;
+    sudoku[2][6] = 1;
+    sudoku[2][7] = 6;
+    sudoku[2][8] = 7;
+    sudoku[3][0] = 3;
+    sudoku[3][1] = 2;
+    sudoku[3][2] = 1;
+    sudoku[3][3] = 4;
+    sudoku[3][4] = 5;
+    sudoku[3][5] = 8;
+    sudoku[3][6] = 7;
+    sudoku[3][7] = 9;
+    sudoku[3][8] = 6;
+    sudoku[4][0] = 7;
+    sudoku[4][1] = 5;
+    sudoku[4][2] = 6;
+    sudoku[4][3] = 2;
+    sudoku[4][4] = 1;
+    sudoku[4][5] = 9;
+    sudoku[4][6] = 3;
+    sudoku[4][7] = 4;
+    sudoku[4][8] = 8;
+    sudoku[5][0] = 4;
+    sudoku[5][1] = 9;
+    sudoku[5][2] = 8;
+    sudoku[5][3] = 7;
+    sudoku[5][4] = 3;
+    sudoku[5][5] = 6;
+    sudoku[5][6] = 5;
+    sudoku[5][7] = 2;
+    sudoku[5][8] = 1;
+    sudoku[6][0] = 8;
+    sudoku[6][1] = 7;
+    sudoku[6][2] = 3;
+    sudoku[6][3] = 5;
+    sudoku[6][4] = 6;
+    sudoku[6][5] = 4;
+    sudoku[6][6] = 9;
+    sudoku[6][7] = 1;
+    sudoku[6][8] = 2;
+    sudoku[7][0] = 9;
+    sudoku[7][1] = 6;
+    sudoku[7][2] = 4;
+    sudoku[7][3] = 1;
+    sudoku[7][4] = 2;
+    sudoku[7][5] = 7;
+    sudoku[7][6] = 8;
+    sudoku[7][7] = 5;
+    sudoku[7][8] = 3;
+    sudoku[8][0] = 2;
+    sudoku[8][1] = 1;
+    sudoku[8][2] = 5;
+    sudoku[8][3] = 8;
+    sudoku[8][4] = 9;
+    sudoku[8][5] = 3;
+    sudoku[8][6] = 6;
+    sudoku[8][7] = 7;
+    sudoku[8][8] = 4;
 }
 void display() //look for sudoku
 {
@@ -155,9 +240,26 @@ void insert_number() //filling sudoku
     int value;
     std::cin >> value;
 
-    std::string str_value = std::to_string(value);
+    sudoku[int_i][int_j] = value;
+}
+void is_finished() //checking that sudoku is finished
+{
+    int val;
 
-    sudoku[int_i][int_j] = str_value;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            val = sudoku[i][j];
+            if (val == 0)
+            {
+                finished = 0;
+                break;
+            }
+            else
+                finished = 1;
+        }
+    }
 }
 
 int main() //main program
@@ -179,6 +281,12 @@ back:
         case 1:
             easy_lvl();
             display();
+            is_finished();
+            while (finished == false)
+            {
+                insert_number();
+                display();
+            }
             break;
         case 2:
             break;
