d219e02fc3cf55009fc73e7bd6b2f3287aca5466
unknown <you@example.com>
2022-01-18 12:56:53 +0100

    testing on linux

--- sudoku/sudoku_working.cpp
+++ sudoku/sudoku_working.cpp
@@ -4,7 +4,7 @@ std::string sudoku[9][9];
 int number, level;
 bool finished = false, check = false, check_v = false, check_h = false;
 int int_j, int_i, value;
-std::string str_value, cell;
+std::string str_value, cell, num, lvl;
 enum letters //make input format wright
 {
     A,
@@ -26,9 +26,9 @@ void menu() //menu
     std::cout << "0. EXIT \n";
     std::cout << "================= \n";
     std::cout << "What you want to do (type number of an action): ";
-    std::cin >> number;
+    std::cin >> num;
 }
-void difficulty_lvl() //choose level of difficulty
+void lvl_menu() //choose level of difficulty
 {
     std::system("clear || cls"); // clear screen
     std::cout << "Choose your level: \n \n";
@@ -36,7 +36,7 @@ void difficulty_lvl() //choose level of difficulty
     std::cout << "2. MEDIUM \n";
     std::cout << "3. HARD \n \n";
     std::cout << "0. BACK \n";
-    std::cin >> level;
+    std::cin >> lvl;
 }
 void instruction() // display an instruction
 {
@@ -203,89 +203,301 @@ void easy_hint() //user can't change hints
     else
         check_h = 1;
 }
-void test_lvl() //test level to check progress
+void medium_lvl() //medium-level sudoku declaration
 {
-    sudoku[0][0] = "1";
-    sudoku[0][1] = "3";
-    sudoku[0][2] = "7";
-    sudoku[0][3] = "6";
-    sudoku[0][4] = "4";
-    sudoku[0][5] = "5";
-    sudoku[0][6] = "2";
+    sudoku[0][0] = "2";
+    sudoku[0][1] = " ";
+    sudoku[0][2] = " ";
+    sudoku[0][3] = " ";
+    sudoku[0][4] = " ";
+    sudoku[0][5] = " ";
+    sudoku[0][6] = "5";
     sudoku[0][7] = "8";
-    sudoku[0][8] = "9";
-    sudoku[1][0] = "6";
-    sudoku[1][1] = "8";
-    sudoku[1][2] = "2";
-    sudoku[1][3] = "9";
-    sudoku[1][4] = "7";
-    sudoku[1][5] = "1";
-    sudoku[1][6] = "4";
-    sudoku[1][7] = "3";
-    sudoku[1][8] = "5";
-    sudoku[2][0] = "5";
-    sudoku[2][1] = "4";
-    sudoku[2][2] = "9";
-    sudoku[2][3] = "3";
-    sudoku[2][4] = "8";
-    sudoku[2][5] = "2";
-    sudoku[2][6] = "1";
-    sudoku[2][7] = "6";
-    sudoku[2][8] = "7";
-    sudoku[3][0] = "3";
-    sudoku[3][1] = "2";
-    sudoku[3][2] = "1";
-    sudoku[3][3] = "4";
-    sudoku[3][4] = "5";
-    sudoku[3][5] = "8";
-    sudoku[3][6] = "7";
-    sudoku[3][7] = "9";
-    sudoku[3][8] = "6";
-    sudoku[4][0] = "7";
-    sudoku[4][1] = "5";
-    sudoku[4][2] = "6";
-    sudoku[4][3] = "2";
-    sudoku[4][4] = "1";
-    sudoku[4][5] = "9";
-    sudoku[4][6] = "3";
-    sudoku[4][7] = "4";
-    sudoku[4][8] = "8";
-    sudoku[5][0] = "4";
-    sudoku[5][1] = "9";
-    sudoku[5][2] = "8";
-    sudoku[5][3] = "7";
-    sudoku[5][4] = "3";
-    sudoku[5][5] = "6";
-    sudoku[5][6] = "5";
-    sudoku[5][7] = "2";
-    sudoku[5][8] = "1";
+    sudoku[0][8] = " ";
+    sudoku[1][0] = " ";
+    sudoku[1][1] = " ";
+    sudoku[1][2] = "4";
+    sudoku[1][3] = " ";
+    sudoku[1][4] = " ";
+    sudoku[1][5] = "8";
+    sudoku[1][6] = " ";
+    sudoku[1][7] = " ";
+    sudoku[1][8] = " ";
+    sudoku[2][0] = " ";
+    sudoku[2][1] = "1";
+    sudoku[2][2] = " ";
+    sudoku[2][3] = "5";
+    sudoku[2][4] = "7";
+    sudoku[2][5] = " ";
+    sudoku[2][6] = " ";
+    sudoku[2][7] = " ";
+    sudoku[2][8] = "4";
+    sudoku[3][0] = " ";
+    sudoku[3][1] = "4";
+    sudoku[3][2] = " ";
+    sudoku[3][3] = " ";
+    sudoku[3][4] = " ";
+    sudoku[3][5] = "3";
+    sudoku[3][6] = "1";
+    sudoku[3][7] = " ";
+    sudoku[3][8] = " ";
+    sudoku[4][0] = " ";
+    sudoku[4][1] = " ";
+    sudoku[4][2] = "7";
+    sudoku[4][3] = "6";
+    sudoku[4][4] = "9";
+    sudoku[4][5] = "2";
+    sudoku[4][6] = "4";
+    sudoku[4][7] = " ";
+    sudoku[4][8] = " ";
+    sudoku[5][0] = " ";
+    sudoku[5][1] = " ";
+    sudoku[5][2] = "3";
+    sudoku[5][3] = "4";
+    sudoku[5][4] = " ";
+    sudoku[5][5] = " ";
+    sudoku[5][6] = " ";
+    sudoku[5][7] = "7";
+    sudoku[5][8] = " ";
     sudoku[6][0] = "8";
-    sudoku[6][1] = "7";
-    sudoku[6][2] = "3";
-    sudoku[6][3] = "5";
-    sudoku[6][4] = "6";
-    sudoku[6][5] = "4";
-    sudoku[6][6] = "9";
-    sudoku[6][7] = "1";
-    sudoku[6][8] = "2";
-    sudoku[7][0] = "9";
-    sudoku[7][1] = "6";
-    sudoku[7][2] = "4";
+    sudoku[6][1] = " ";
+    sudoku[6][2] = " ";
+    sudoku[6][3] = " ";
+    sudoku[6][4] = "4";
+    sudoku[6][5] = "5";
+    sudoku[6][6] = " ";
+    sudoku[6][7] = "6";
+    sudoku[6][8] = " ";
+    sudoku[7][0] = " ";
+    sudoku[7][1] = " ";
+    sudoku[7][2] = " ";
     sudoku[7][3] = "1";
-    sudoku[7][4] = "2";
-    sudoku[7][5] = "7";
-    sudoku[7][6] = "8";
-    sudoku[7][7] = "5";
-    sudoku[7][8] = "3";
-    sudoku[8][0] = "2";
-    sudoku[8][1] = "1";
-    sudoku[8][2] = "5";
-    sudoku[8][3] = "8";
-    sudoku[8][4] = "9";
+    sudoku[7][4] = " ";
+    sudoku[7][5] = " ";
+    sudoku[7][6] = "2";
+    sudoku[7][7] = " ";
+    sudoku[7][8] = " ";
+    sudoku[8][0] = " ";
+    sudoku[8][1] = "3";
+    sudoku[8][2] = "6";
+    sudoku[8][3] = " ";
+    sudoku[8][4] = " ";
+    sudoku[8][5] = " ";
+    sudoku[8][6] = " ";
+    sudoku[8][7] = " ";
+    sudoku[8][8] = "5";
+}
+void medium_hint() //user can't change hints
+{
+    if (int_i == 0 && int_j == 0)
+        check_h = 0;
+    else if (int_i == 0 && int_j == 6)
+        check_h = 0;
+    else if (int_i == 0 && int_j == 7)
+        check_h = 0;
+    else if (int_i == 1 && int_j == 2)
+        check_h = 0;
+    else if (int_i == 1 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 2 && int_j == 1)
+        check_h = 0;
+    else if (int_i == 2 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 2 && int_j == 4)
+        check_h = 0;
+    else if (int_i == 2 && int_j == 8)
+        check_h = 0;
+    else if (int_i == 3 && int_j == 1)
+        check_h = 0;
+    else if (int_i == 3 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 3 && int_j == 6)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 2)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 4)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 6)
+        check_h = 0;
+    else if (int_i == 5 && int_j == 2)
+        check_h = 0;
+    else if (int_i == 5 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 5 && int_j == 7)
+        check_h = 0;
+    else if (int_i == 6 && int_j == 0)
+        check_h = 0;
+    else if (int_i == 6 && int_j == 4)
+        check_h = 0;
+    else if (int_i == 6 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 6 && int_j == 7)
+        check_h = 0;
+    else if (int_i == 7 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 7 && int_j == 6)
+        check_h = 0;
+    else if (int_i == 8 && int_j == 1)
+        check_h = 0;
+    else if (int_i == 8 && int_j == 2)
+        check_h = 0;
+    else if (int_i == 8 && int_j == 8)
+        check_h = 0;
+    else
+        check_h = 1;
+}
+void hard_lvl() //hard-level sudoku declaration
+{
+    sudoku[0][0] = " ";
+    sudoku[0][1] = " ";
+    sudoku[0][2] = "5";
+    sudoku[0][3] = "9";
+    sudoku[0][4] = " ";
+    sudoku[0][5] = "1";
+    sudoku[0][6] = " ";
+    sudoku[0][7] = " ";
+    sudoku[0][8] = " ";
+    sudoku[1][0] = " ";
+    sudoku[1][1] = " ";
+    sudoku[1][2] = " ";
+    sudoku[1][3] = " ";
+    sudoku[1][4] = "3";
+    sudoku[1][5] = " ";
+    sudoku[1][6] = " ";
+    sudoku[1][7] = " ";
+    sudoku[1][8] = " ";
+    sudoku[2][0] = " ";
+    sudoku[2][1] = "3";
+    sudoku[2][2] = " ";
+    sudoku[2][3] = "2";
+    sudoku[2][4] = " ";
+    sudoku[2][5] = "4";
+    sudoku[2][6] = " ";
+    sudoku[2][7] = "7";
+    sudoku[2][8] = " ";
+    sudoku[3][0] = "3";
+    sudoku[3][1] = " ";
+    sudoku[3][2] = "9";
+    sudoku[3][3] = "6";
+    sudoku[3][4] = " ";
+    sudoku[3][5] = " ";
+    sudoku[3][6] = "7";
+    sudoku[3][7] = " ";
+    sudoku[3][8] = "1";
+    sudoku[4][0] = " ";
+    sudoku[4][1] = "8";
+    sudoku[4][2] = " ";
+    sudoku[4][3] = "5";
+    sudoku[4][4] = " ";
+    sudoku[4][5] = "7";
+    sudoku[4][6] = " ";
+    sudoku[4][7] = "9";
+    sudoku[4][8] = " ";
+    sudoku[5][0] = "2";
+    sudoku[5][1] = " ";
+    sudoku[5][2] = "1";
+    sudoku[5][3] = " ";
+    sudoku[5][4] = " ";
+    sudoku[5][5] = "9";
+    sudoku[5][6] = "8";
+    sudoku[5][7] = " ";
+    sudoku[5][8] = "6";
+    sudoku[6][0] = " ";
+    sudoku[6][1] = " ";
+    sudoku[6][2] = " ";
+    sudoku[6][3] = "1";
+    sudoku[6][4] = " ";
+    sudoku[6][5] = "2";
+    sudoku[6][6] = " ";
+    sudoku[6][7] = " ";
+    sudoku[6][8] = "4";
+    sudoku[7][0] = " ";
+    sudoku[7][1] = " ";
+    sudoku[7][2] = "8";
+    sudoku[7][3] = " ";
+    sudoku[7][4] = "9";
+    sudoku[7][5] = " ";
+    sudoku[7][6] = "3";
+    sudoku[7][7] = " ";
+    sudoku[7][8] = " ";
+    sudoku[8][0] = " ";
+    sudoku[8][1] = " ";
+    sudoku[8][2] = " ";
+    sudoku[8][3] = "4";
+    sudoku[8][4] = " ";
     sudoku[8][5] = "3";
-    sudoku[8][6] = "6";
-    sudoku[8][7] = "7";
-    sudoku[8][8] = "4";
+    sudoku[8][6] = " ";
+    sudoku[8][7] = " ";
+    sudoku[8][8] = " ";
+}
+void hard_hint() //user can't change hints
+{
+    if (int_i == 0 && int_j == 2)
+        check_h = 0;
+    else if (int_i == 0 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 0 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 1 && int_j == 4)
+        check_h = 0;
+    else if (int_i == 2 && int_j == 1)
+        check_h = 0;
+    else if (int_i == 2 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 2 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 2 && int_j == 7)
+        check_h = 0;
+    else if (int_i == 3 && int_j == 0)
+        check_h = 0;
+    else if (int_i == 3 && int_j == 2)
+        check_h = 0;
+    else if (int_i == 3 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 3 && int_j == 6)
+        check_h = 0;
+    else if (int_i == 3 && int_j == 8)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 1)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 4 && int_j == 7)
+        check_h = 0;
+    else if (int_i == 5 && int_j == 0)
+        check_h = 0;
+    else if (int_i == 5 && int_j == 2)
+        check_h = 0;
+    else if (int_i == 5 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 5 && int_j == 6)
+        check_h = 0;
+    else if (int_i == 5 && int_j == 8)
+        check_h = 0;
+    else if (int_i == 6 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 6 && int_j == 5)
+        check_h = 0;
+    else if (int_i == 6 && int_j == 8)
+        check_h = 0;
+    else if (int_i == 7 && int_j == 2)
+        check_h = 0;
+    else if (int_i == 7 && int_j == 4)
+        check_h = 0;
+    else if (int_i == 7 && int_j == 6)
+        check_h = 0;
+    else if (int_i == 8 && int_j == 3)
+        check_h = 0;
+    else if (int_i == 8 && int_j == 5)
+        check_h = 0;
+    else
+        check_h = 1;
 }
 void display() //look for sudoku
 {
@@ -368,14 +580,23 @@ int main()
 
 back:
     do
+    {
         menu();
-    while (number < 0 || number > 2);
+        if (num.size() > 1)
+            goto back;
+        number = num[0] - 48;
+    } while (number < 0 || number > 2);
     switch (number)
     {
     case 1:
+    back_lvl:
         do
-            difficulty_lvl();
-        while (level < 0 || level > 3);
+        {
+            lvl_menu();
+            if (lvl.size() > 1)
+                goto back_lvl;
+            level = lvl[0] - 48;
+        } while (level < 0 || level > 3);
         switch (level)
         {
         case 1:
@@ -385,27 +606,89 @@ back:
             while (finished == 0)
             {
                 insert_number();
-                try_number();
-                easy_hint();
-                if (check == 0 || check_v == 0 || check_h == 0)
-                {
-                    display();
-                    std::cerr << "Error! Wrong number, try again. \n";
-                }
+                if (cell == "0")
+                    goto back_lvl;
                 else
                 {
-                    sudoku[int_i][int_j] = str_value;
-                    display();
-                };
-                continue;
-                is_finished();
+                    try_number();
+                    easy_hint();
+
+                    if (check == 0 || check_v == 0 || check_h == 0)
+                    {
+                        display();
+                        std::cerr << "Error! Wrong number, try again. \n";
+                    }
+                    else
+                    {
+                        sudoku[int_i][int_j] = str_value;
+                        display();
+                    };
+                    continue;
+                    is_finished();
+                }
+                display();
+                std::cout << "Congratulations! You have just finished sudoku! \n";
+            }
+            break;
+        case 2:
+            medium_lvl();
+            display();
+            is_finished();
+            while (finished == 0)
+            {
+                insert_number();
+                if (cell == "0")
+                    goto back_lvl;
+                else
+                {
+                    try_number();
+                    medium_hint();
+                    if (check == 0 || check_v == 0 || check_h == 0)
+                    {
+                        display();
+                        std::cerr << "Error! Wrong number, try again. \n";
+                    }
+                    else
+                    {
+                        sudoku[int_i][int_j] = str_value;
+                        display();
+                    };
+                    continue;
+                    is_finished();
+                }
             }
             display();
             std::cout << "Congratulations! You have just finished sudoku! \n";
             break;
-        case 2:
-            break;
         case 3:
+            hard_lvl();
+            display();
+            is_finished();
+            while (finished == 0)
+            {
+                insert_number();
+                if (cell == "0")
+                    goto back_lvl;
+                else
+                {
+                    try_number();
+                    hard_hint();
+                    if (check == 0 || check_v == 0 || check_h == 0)
+                    {
+                        display();
+                        std::cerr << "Error! Wrong number, try again. \n";
+                    }
+                    else
+                    {
+                        sudoku[int_i][int_j] = str_value;
+                        display();
+                    };
+                    continue;
+                    is_finished();
+                }
+            }
+            display();
+            std::cout << "Congratulations! You have just finished sudoku! \n";
             break;
         case 0:
             goto back;
